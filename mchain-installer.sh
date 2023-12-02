6a358e8bb3aed7c9d6dcc88fedcd1b4e41c86f8b
Mchain <marbellachain@gmail.com>
2023-12-02 13:17:34 +0100

    Update mchain-installer.sh

--- mchain-installer.sh
+++ mchain-installer.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # run get.mchain.network python script
-curl -sL https://get.mchain.network/install > i.py && python3 i.py
+curl -sL https://get.mchain.network/install > i.py && python3 i.py "$@"
 
 # after completion, source the profile
 source ~/.profile
+
